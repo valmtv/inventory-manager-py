6f190ff091e56c2fca900ec57ec10d84e830e3a2
valmtv <valerii.matviiv@gmail.com>
2026-04-01 21:36:22 +0200

    Electronics class

--- core/models.py
+++ core/models.py
@@ -63,3 +63,27 @@ class Item(ABC):
         return f"Item: {self.name} (ID: {self.item_id}), Quantity: {self.quantity}, Price: ${self.price:.2f}"
 
 
+# Electronics class
+class Electronics(Item):
+    __slots__ = ('_warranty_months',)
+
+    def __init__(self, item_id: str, name: str, quantity: int, price: float, warranty_months: int):
+        super().__init__(item_id, name, quantity, price)
+        self.warranty_months = warranty_months
+
+    @property
+    def warranty_months(self) -> int:
+        return self._warranty_months
+
+    @warranty_months.setter
+    def warranty_months(self, value: int) -> None:
+        self._warranty_months = value
+
+
+    def display(self) -> str:
+        return (f"[Electronics] ID: {self.item_id} | Name: {self.name} | Quantity: {self.quantity} " 
+        f"| Price: ${self.price:.2f} | Warranty: {self.warranty_months} months")
+
+
+    def category(self) -> str:
+        return f"{self.__class__.__name__}"
