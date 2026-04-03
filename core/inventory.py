ba2ecd57b59419551e764eb26fbf41f01f60d205
valmtv <valerii.matviiv@gmail.com>
2026-04-03 22:08:33 +0200

    Inventory core methods

--- core/inventory.py
+++ core/inventory.py
@@ -16,3 +16,17 @@ class Inventory:
         # values, not keys
         return iter(self._items.values())
 
+
+    # Core methods
+    def add_item(self, item: Item) -> None:
+        self._items[item.item_id] = item
+
+    def remove_item(self, item_id: str) -> None:
+        del self._items[item_id]
+
+    def update_quantity(self, item_id: str, quantity: int) -> None:
+        self._items[item_id].quantity = quantity
+
+    def display_inventory(self) -> None:
+        for item in self:
+            print(item.display())
