d38346bbcaf513f8c05cd4f57575e0128ca4f0d6
valmtv <valerii.matviiv@gmail.com>
2026-04-05 18:06:37 +0200

    Small correction

--- core/inventory.py
+++ core/inventory.py
@@ -2,7 +2,7 @@ from typing import Iterator
 from core.models import Item
 import functools
 from datetime import datetime
-from core.exceptions import ItemNotFoundException, DuplicateItemException, InvalidValueException
+from core.exceptions import ItemNotFoundException, DuplicateItemException
 
 def log_operation(func):
     @functools.wraps(func)
@@ -32,10 +32,9 @@ class Inventory:
 
 
     # Core methods
-    # TODO: Simple for now, later with exceptions and checks
     @log_operation
     def add_item(self, item: Item) -> None:
-        if item.item_id in self._items:
+        if item.item_id in self: # triggers __contains__
             raise DuplicateItemException(item.item_id, f"Item with ID '{item.item_id}' already exists in inventory.")
         self._items[item.item_id] = item
 
