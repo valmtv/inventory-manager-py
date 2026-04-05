80b5473115dc55419021494c560d2395d91b9e04
valmtv <valerii.matviiv@gmail.com>
2026-04-05 17:45:54 +0200

    Add DuplicateItemException & InvalidValueException

--- core/exceptions.py
+++ core/exceptions.py
@@ -3,6 +3,16 @@ class InventoryException(Exception):
     pass
 
 class ItemNotFoundException(InventoryException):
-    def __init__(self, item_id: str, msg: str = "Error: Item not found."):
+    def __init__(self, item_id: str, msg: str = "Item not found."):
         super().__init__(msg)
         self.item_id = item_id
+
+class DuplicateItemException(InventoryException):
+    def __init__(self, item_id: str, msg: str = "Item with this ID already exists in inventory."):
+        super().__init__(msg)
+        self.item_id = item_id
+
+class InvalidValueException(InventoryException):
+    def __init__(self, msg: str = "Quantity must be a non-negative integer."):
+        super().__init__(msg) 
+
